ed5c2fd0de58cd6673a346892cf95e2705e32b7c
Toni-Ty <antonique2479@yahoo.com>
2019-03-19 19:18:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,8 +7,56 @@ class Song
     @@all
   end
 
+def self.create
+  song = self.new
+  song.save
+  song
+end
+
+def self.new_by_name(song_name)
+  song = self.new
+  song.name = song_name
+  song
+end
+
+def self.create_by_name(song_name)
+  song = self.create
+  song.name = song_name
+  song
+
+end
+
+def self.find_by_name(song_name)
+  self.all.find{ |song| song.name == song_name}
+end
+
+def self.find_or_create_by_name(song_name)
+  find_by_name(song_name) || create_by_name(song_name)
+end
+
+def self.alphabetical
+  self.all.sort_by { |song| song.name}
+end
+
+def self.new_from_filename(filename)
+  title = filename.split(" ")
+  artist = [0]
+  song_name, extension = title[1].split(".")
+
+
+#   song = self.create
+#   song.name =
+# end
+end
+
+
+# def self.destroy_all
+#   self.all.delete
+# end
   def save
     self.class.all << self
   end
+
+
 
 end

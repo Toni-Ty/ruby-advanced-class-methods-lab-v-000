109289402d801773e6bfc803358bad98f5f5f058
Toni-Ty <antonique2479@yahoo.com>
2019-03-19 20:41:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,15 +40,9 @@ end
 
 def self.new_from_filename(filename)
   song = self.new
-  song.name = (filename.split(" - ")[1].chomp(".mp3"))
+  song.fn = (filename.split(" - ")[1].chomp(".mp3"))
   song.artist_name = (filename.split(" - ")[0])
   song
-end
-
-def self.new_from_filename(filename)
-  song = self.new
-  song.name = (filename.split(".")[1].chomp(".mp3"))
-  song.artist_name = (filename.split(" - ")[0])
 end
 
 
